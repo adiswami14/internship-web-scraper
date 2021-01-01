71d8e17e67ab99ef18584056e36c27a52d9dc7cd
adiswami14 <adithya1431@gmail.com>
2021-01-01 02:10:28 -0500

    Finish basic web scraping code with functionality to type in area of interest

--- scraper.py
+++ scraper.py
@@ -1,9 +1,33 @@
 import requests
+from bs4 import BeautifulSoup
 
 headers = {'User-Agent': 
     'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.83 Safari/537.36'
     }
 
-url = 'https://www.monster.com/jobs/search/?q=Software-Developer-Intern&where=New-Jersey' #monster job site
+
+internship_loc = input("Where are you searching for an internship?")
+for char in internship_loc:
+    if char == ' ':
+        char = '-'
+
+url = 'https://www.monster.com/jobs/search/?q=Software-Developer-Intern&where='+internship_loc #monster job site
 page = requests.get(url, headers = headers)
-print(page.text)
+
+soup = BeautifulSoup(page.content, 'html.parser')
+results = soup.find(id = 'ResultsContainer')
+
+internships = results.find_all('section', class_='card-content')
+
+for internship in internships:
+    title = internship.find('h2', class_='title')
+    company = internship.find('div', class_='company')
+    location = internship.find('div', class_='location')
+
+    if None in (title, company, location):
+        continue
+
+    print(title.text.strip())
+    print(company.text.strip())
+    print(location.text.strip())
+    print()
